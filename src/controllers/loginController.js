657c6fd1c76c39ee8f53ae8464a84f287e054e66
MauroAmpuero <mau999ampu@gmail.com>
2021-12-05 17:12:00 -0300

    nuevo repo

---
+++ src/controllers/loginController.js
@@ -0,0 +1,107 @@
+const User = require("../models/users.js")
+const bcryptjs = require("bcryptjs")
+const { validationResult } = require("express-validator")
+let db= require("../../database/models");
+const loginController=
+    {entrarLogin:(req,res)=>{
+        res.render("login")
+    },
+
+    processLogin:  (req, res) => {
+        /*let errors = validationResult(req);
+        if (errors.isEmpty){
+
+        }else {
+            return res.render('login', {errors: errors.errors})
+        }*/
+         db.User.findOne({
+            where: {email:req.body.email}
+        }).then(function async (userToLogin){
+            if(await userToLogin.password === req.body.password){
+                req.session.userLogged = userToLogin;
+                res.redirect('/login/perfil')
+            }
+            
+            /*
+            if(passwordCorrect){
+                let passwordCorrect = bcryptjs.compareSync(req.body.password, userToLogin.password)
+                if(passwordCorrect){
+                    req.session.userLogged = userToLogin;
+                    return res.redirect('/login/perfil')
+                }
+                return res.render('login',{
+                    errors: {
+                        email: {
+                            msg: "Email o Contraseña son incorrectos"
+                        }
+                    }
+                })
+            }
+            req.session.userToLogin = userLogued;
+            return res.render('login',{
+                errors: {
+                    email: {
+                        msg: "Email inválido"
+                    }
+                }
+            })
+        */})
+        /*if(userToLogin){
+            let passwordCorrect = bcryptjs.compareSync(req.body.password, userToLogin.password)
+            if(passwordCorrect){
+                req.session.userLogged = userToLogin;
+                return res.redirect('/login/perfil')
+            }
+            return res.render('login',{
+                errors: {
+                    email: {
+                        msg: "Email o Contraseña son incorrectos"
+                    }
+                }
+            })
+        }
+        req.session.userToLogin = userLogued;
+        return res.render('login',{
+            errors: {
+                email: {
+                    msg: "Email inválido"
+                }
+            }
+        })*/
+
+        /* let userToLogin = User.findByField('email', req.body.email);
+		if(userToLogin){
+            let passwordCorrect = bcryptjs.compareSync(req.body.password, userToLogin.password)
+            if(passwordCorrect){
+                req.session.userLogged = userToLogin;
+                return res.redirect("/login/perfil")
+            }
+            return res.render('login',{
+                errors: {
+                    email: {
+                        msg: "Email o Contraseña son incorrectos"
+                    }
+                }
+            })
+            
+        }
+        req.session.userToLogin = userLogued;
+        return res.render('login',{
+            errors: {
+                email: {
+                    msg: "Email inválido"
+                }
+            }
+        })*/
+		
+	},
+    profile: (req,res)=>{
+        res.render("perfil",{user: req.session.userLogged} )
+    },
+    logout: (req,res)=>{
+        req.session.destroy()
+        return res.redirect("/")
+    }
+}
+module.exports= loginController;
+
